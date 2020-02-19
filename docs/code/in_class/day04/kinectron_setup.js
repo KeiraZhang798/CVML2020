486fb6f55d33f585f94e53b484c24cd4fc293871
KeiraZhang798 <60228893+KeiraZhang798@users.noreply.github.com>
2020-02-19 05:20:15 -0600

    set up website+kinestron

---
+++ docs/code/in_class/day04/kinectron_setup.js
@@ -0,0 +1,35 @@
+
+let w=640;
+let h=480;
+
+let kinectron;
+
+function setup(){
+let canvas=createCanvas(w,h);
+canvas.parent("#sketch");
+background(0);
+
+kinectron=new Kinectron("10.75.26.216");
+
+kinectron.makeConnection();
+
+kinectron.startTrackedBodies(drawBody);
+
+}
+
+function draw(){
+
+
+
+}
+
+function drawBody(body){
+    background(0);
+    for(let i=0; i<body.joints.length; i++){
+
+        fill(255,0,0);
+        ellipse(body.joints[i].depthX*width,body.joints[i].depthY*height,20,20);
+ 
+    }
+}
+
